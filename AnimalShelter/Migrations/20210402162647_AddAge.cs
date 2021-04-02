439b6f092a5864cb6c4eeb680b6b1ac7fdc17c17
Nick Lindau <nicholaithegreat@gmail.com>
2021-04-02 09:27:35 -0700

    Add migration to add age to animal

---
+++ AnimalShelter/Migrations/20210402162647_AddAge.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class AddAge : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Animals",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Animals");
+        }
+    }
+}
--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -5,6 +5,7 @@ namespace AnimalShelter.Models
     public int AnimalId { get; set; }
     public string Name { get; set; }
     public string Type { get; set; }
+    public int Age  { get; set; }
     public string Breed { get; set; }
     public string Description { get; set; }
   }
