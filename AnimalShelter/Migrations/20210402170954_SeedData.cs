fc52398ee89c16d85b7c8be5d7138458f788979d
Nick Lindau <nicholaithegreat@gmail.com>
2021-04-02 10:10:39 -0700

    add migration for seed data and finish endpoints for controller

---
+++ AnimalShelter/Migrations/20210402170954_SeedData.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Breed", "Description", "Gender", "Name", "Type" },
+                values: new object[,]
+                {
+                    { 1, 9, "Chocoloate Lab", "Spayed, needs a loving home with older childred/no children", "Female", "Ted", "Cat" },
+                    { 2, 2, "Pug", "Neutered, spunky and likes a lot of attention", "Male", "Bradley", "Dog" },
+                    { 3, 3, "Husky", "Neutered, needs a LOT of exercise", "Male", "Ghost", "Dog" },
+                    { 4, 1, "Australian Shepherd", "BNeutered, needs to be given tasks for feel important", "Male", "Sparky", "Dog" },
+                    { 5, 9, "Persian", "Spayed, Very shy. Better suited to a home with no/older children", "Female", "Molly", "Cat" },
+                    { 6, 1, "Tabby(Orange)", "Neutered, very active loves to play", "Male", "Jose", "Cat" },
+                    { 7, 3, "Great Dane", "Neutered, dumb sweetheart. Needs a lot of room to move around", "Male", "Thor", "Dog" },
+                    { 8, 2, "Chocoloate Lab", "Spayed, loves playing fetch and being active", "Female", "Charlie", "Dog" },
+                    { 9, 2, "Calico", "Spayed, waiting for owner pickup", "Female", "Chloe", "Cat" },
+                    { 10, 9, "English Bulldog", "Neutered, will need surgery on knees and potentially nose", "Male", "Rocket", "Dog" },
+                    { 11, 4, "Persian", "Spayed, loves sleeping doesn't mind children", "Female", "Tina Turner", "Cat" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 11);
+        }
+    }
+}
--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -9,5 +9,23 @@ namespace AnimalShelter.Models
       {
       }
     public DbSet<Animal> Animals { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Animal>()
+        .HasData(
+          new Animal { AnimalId = 1, Name = "Ted", Type = "Cat", Age = 9, Gender = "Female", Breed = "Chocoloate Lab", Description = "Spayed, needs a loving home with older childred/no children"},
+          new Animal { AnimalId = 2, Name = "Bradley", Type = "Dog", Age = 2, Gender = "Male", Breed = "Pug", Description = "Neutered, spunky and likes a lot of attention"},
+          new Animal { AnimalId = 3, Name = "Ghost", Type = "Dog", Age = 3, Gender = "Male", Breed = "Husky", Description = "Neutered, needs a LOT of exercise"},
+          new Animal { AnimalId = 4, Name = "Sparky", Type = "Dog", Age = 1, Gender = "Male", Breed = "Australian Shepherd", Description = "BNeutered, needs to be given tasks for feel important"},
+          new Animal { AnimalId = 5, Name = "Molly", Type = "Cat", Age = 9, Gender = "Female", Breed = "Persian", Description = "Spayed, Very shy. Better suited to a home with no/older children"},
+          new Animal { AnimalId = 6, Name = "Jose", Type = "Cat", Age = 1, Gender = "Male", Breed = "Tabby(Orange)", Description = "Neutered, very active loves to play"},
+          new Animal { AnimalId = 7, Name = "Thor", Type = "Dog", Age = 3, Gender = "Male", Breed = "Great Dane", Description = "Neutered, dumb sweetheart. Needs a lot of room to move around"},
+          new Animal { AnimalId = 8, Name = "Charlie", Type = "Dog", Age = 2, Gender = "Female", Breed = "Chocoloate Lab", Description = "Spayed, loves playing fetch and being active"},
+          new Animal { AnimalId = 9, Name = "Chloe", Type = "Cat", Age = 2, Gender = "Female", Breed = "Calico", Description = "Spayed, waiting for owner pickup"},
+          new Animal { AnimalId = 10, Name = "Rocket", Type = "Dog", Age = 9, Gender = "Male", Breed = "English Bulldog", Description = "Neutered, will need surgery on knees and potentially nose"},
+          new Animal { AnimalId = 11, Name = "Tina Turner", Type = "Cat", Age = 4, Gender = "Female", Breed = "Persian", Description = "Spayed, loves sleeping doesn't mind children"}          
+        );
+    }
   }
 }
